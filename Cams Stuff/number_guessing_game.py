31b198d13bc678a99fe009c3fd0bd8982ea635f5
DJHook98 <danhook@live.co.uk>
2014-02-20 14:49:49 +0000

    Fixed Cameron's Dodgy Code
    * Deleted data.txt
    * Added comments
    * Fixed printing
    * Fixed format

--- Cams Stuff/number_guessing_game.py
+++ Cams Stuff/number_guessing_game.py
@@ -1,10 +1,17 @@
 #Number Guessing Game#
+
+# Import modules
 import random
+
+# Define variables
 trys = 0
-print("Welcome to Cameron's number guessing game!")
 number = random.randint(0,11)
+
+# Intro
+print("Welcome to Cameron's number guessing game!")
+print("I'm thinking of a number between 1 and 10, what number is it?")
 while trys < 3:
-    user = int(input("I'm thinking of a number between 1 and 10 what number is it"))
+    user = int(input("Have a guess: "))
     if user == number:
         print("Well done you guessed correctly!")
         print("Game over")
