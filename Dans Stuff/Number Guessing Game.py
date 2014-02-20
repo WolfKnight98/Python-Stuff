f8f82c142de591b4f3f13b941959d9da25c47945
DJHook98 <danhook@live.co.uk>
2014-02-20 14:39:39 +0000

    Added Dans Number Guessing Game

---
+++ Dans Stuff/Number Guessing Game.py
@@ -0,0 +1,30 @@
+# Dan's number guessing game
+
+# Import modules
+import random
+
+# Intro
+print("Welcome to Dan's number guessing game!\n")
+
+# Define variables
+guesses = 0
+number = random.randint(0, 11)
+
+# Main program loop
+while( guesses < 3 ):
+    u_guess = int(input("Have a guess: "))
+
+    if u_guess == number:
+        print("Well done, you win!")
+        break
+    elif u_guess > number:
+        print("Your guess was too high.")
+        guesses = guesses + 1
+    elif u_guess < number:
+        print("Your guess was too low.")
+        guesses = guesses + 1
+    else:
+        print("There was an error.")
+
+if guesses == 3:
+    print("You lose!")
