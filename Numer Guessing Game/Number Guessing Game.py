8485840b94e02ee739657642ebaf3d80d4f4df87
DJHook98 <danhook@live.co.uk>
2014-04-21 14:45:05 +0100

    Just added spaces everywhere because lua takes over your mind

--- Numer Guessing Game/Number Guessing Game.py
+++ Numer Guessing Game/Number Guessing Game.py
@@ -4,32 +4,32 @@
 import random
 
 # Intro
-print("Welcome to Dan's number guessing game!\n")
+print( "Welcome to Dan's number guessing game!\n" )
 
 # Define variables
 guesses = 0
 allowed_guesses = 3
-number = random.randint(0, 10)
+number = random.randint( 0, 10 )
 
 # Main program loop
-while(guesses < 3):
+while( guesses < 3 ):
     guess = input(
-        "Choose a number between 0 and 10 (%s guesses remaining): " % (allowed_guesses - guesses)
+        "Choose a number between 0 and 10 (%s guesses remaining): " % ( allowed_guesses - guesses )
     )
-    u_guess = int(guess)
+    u_guess = int( guess )
 
     if u_guess == number:
-        print("OMG, how did you guess, you win and are cool!")
+        print( "OMG, how did you guess, you win and are cool!" )
         break
     elif u_guess > number:
-        print("Your guess was too high.")
+        print( "Your guess was too high." )
         guesses = guesses + 1
     elif u_guess < number:
-        print("Your guess was too low.")
+        print( "Your guess was too low." )
         guesses = guesses + 1
     else:
-        print("There was an error.")
+        print( "There was an error." )
 
 if guesses == allowed_guesses:
-    print("You lose!")
-    print("The number was: " + str(number))
+    print( "You lose!" )
+    print( "The number was: " + str( number ) )
