42c65cf749476ed704ca92b48f0fb23c08d56ae8
DJHook98 <danhook@live.co.uk>
2014-05-03 12:29:59 +0100

    :D

--- ATM Machine/atm_data.py
+++ ATM Machine/atm_data.py
@@ -3,7 +3,7 @@
 
 error = "There was an error, contact the developer."
 
-menu = [ "Welcome to Dan's bank system version 0.3a", "Your options are:", 
+menu = [ "Welcome to Dan and Tom's bank system version 0.3a", "Your options are:", 
         "    (1) - Withdraw funds", 
         "    (2) - Deposit funds", 
         "    (3) - Exit" ]
